27854c25f01e1f7b453c364573126cb32b2d74e3
Leckar <gusleckar@gmail.com>
2022-05-30 16:58:08 -0300

    100% complete

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 // Variáveis necessárias para que a aplicação funcione;
-const classes = ['newspaper', 'magazine1', 'magazine2', 'medium',
-  'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+const paperType = ['newspaper', 'magazine1', 'magazine2'];
+const fontSize = ['medium', 'big', 'reallybig'];
+const rotation = ['rotateleft', 'rotateright'];
+const slanting = ['skewleft', 'skewright'];
+const allClasses = [paperType, fontSize, rotation, slanting];
 const button = document.getElementById('criar-carta');
 const inputText = document.getElementById('carta-texto');
 const wordCount = document.getElementById('carta-contador');
 const paragraph = document.getElementById('carta-gerada');
 const spanCollection = document.getElementsByTagName('span');
 // Função que gera um índice aleatório;
-function randomIndexGenerator() {
-  return Math.round(Math.random() * 9);
+function randomIndexGenerator(n) {
+  const rng = Math.round(Math.random() * n);
+  return rng;
+}
+// Funções que retornam uma classe de seu respectivo tipo aleatória;
+function classSetter() {
+  const newArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    const arrayGetter = allClasses[i];
+    const newIndex = randomIndexGenerator(arrayGetter.length - 1);
+    newArray[i] = (arrayGetter[newIndex]);
+  }
+  console.log(newArray);
+  return newArray;
 }
 // Função que gera o span e atribui uma classe aleatoriamente a ele;
 function spanGenerator() {
-  const index = randomIndexGenerator();
   const newSpan = document.createElement('span');
-  newSpan.classList.add(classes[index]);
+  const newClassArray = classSetter();
+  for (let i = 0; i < newClassArray.length; i += 1) {
+    newSpan.classList.add(newClassArray[i]);
+  }
   return newSpan;
 }
 // Função que recebe o valor do input e separa os elementos;
@@ -23,16 +40,29 @@ function valueConverter() {
   const valueArray = inputValue.split(' ');
   return valueArray;
 }
+// Função que compara a classe atual com a nova gerada e remove a anterior;
+function classResetter(span) {
+  const spanClasses = span.classList;
+  const newArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    const arrayGetter = allClasses[i];
+    let newIndex = randomIndexGenerator(arrayGetter.length - 1);
+    while (spanClasses[i] === arrayGetter[newIndex]) {
+      newIndex = randomIndexGenerator(arrayGetter.length - 1);
+    }
+    newArray.push(arrayGetter[newIndex]);
+  }
+  return newArray;
+}
 // Função handler do event listener dos spans;
 function spanHandler(e) {
-  let index = randomIndexGenerator();
   const span = e;
-  const removedClass = span.classList[0];
-  if (removedClass === classes[index]) {
-    index = Math.abs(index - 1);
+  const spanClasses = e.classList;
+  const newClassesArray = classResetter(span);
+  for (let i = 3; i >= 0; i -= 1) {
+    spanClasses.remove(spanClasses[i]);
+    spanClasses.add(newClassesArray[3 - i]);
   }
-  span.classList.remove(removedClass);
-  span.classList.add(classes[index]);
 }
 // Função que cria o event listener dos spans;
 function spanListener() {
